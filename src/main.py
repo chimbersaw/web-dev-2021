1568f09ca2fb2aa1ccf9452703e8aa55868e2f4a
Maxim <maksukh@inbox.ru>
2021-09-22 17:13:12 +0300

    Refactor code structure

--- src/main.py
+++ src/main.py
@@ -1,25 +1,7 @@
 from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
 
-from src.models.item import Item
+from src.routers import api
 
 app = FastAPI()
 
-
-@app.get("/", response_class=PlainTextResponse)
-def read_root():
-    return "Hello world!"
-
-
-@app.get("/api/hello", response_class=PlainTextResponse)
-def say_hello(username: str = "world"):
-    return "Hello {}!".format(username)
-
-
-@app.post("/api/items/")
-def update_item(item: Item):
-    return Item(
-        username=item.username,
-        price=item.price * 0.8,
-        item_name=item.item_name + " (with discount)"
-    )
+app.include_router(api.router)
